0e1bf5c2439d551faae1bd9d4b940be15616c3d8
laskor <al.moksud@gmail.com>
2022-11-13 12:52:21 -0500

    scroll to top added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ScrollToTop from "react-scroll-to-top";
+import { FaLevelUpAlt } from "react-icons/fa";
 
 //Pages
 import Home from "./pages/Home";
@@ -11,12 +13,14 @@ import './assets/css/main.css';
 import './assets/css/responsive.css';
 import './assets/vendor/modal-video/modal-video.min.css';
 import './assets/vendor/slick/slick.css';
+
 function App() {
   return (
     <Router>
         <Routes>
             <Route path="/" exact element={<Home />} />
         </Routes>
+        <ScrollToTop className="scrollUp" smooth top="1500" component={<FaLevelUpAlt />} />    
     </Router>
   );
 }
